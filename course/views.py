7557d348f2a5b97c766d38f63670a6750f127e94
kadai <kadai0308@gmail.com>
2017-01-30 17:43:47 +0800

    fix add course

--- course/views.py
+++ course/views.py
@@ -8,20 +8,22 @@ import json
 import requests
 
 def add_course (request):
-    all_courses = requests.get('https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/JH/OPENDATA/open_course_data_10510.json').text
-    all_courses = json.loads(all_courses)
-    for course in all_courses:
-        dep = course['科號'][5:9].replace(' ','')
-        Course.objects.get_or_create(
-            course_no = course['科號'],
-            defaults = {
-                'course_no': course['科號'],
-                'title_tw': course['課程中文名稱'],
-                'title_en': course['課程英文名稱'],
-                'credit': course['學分數'],
-                'teacher': course['授課教師'],
-                'department': dep,
-            }
-        )
+    urls = ['https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/JH/OPENDATA/open_course_data.json', 'https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/JH/OPENDATA/open_course_data_10510.json']
+    for url in urls:
+        all_courses = requests.get(url).text
+        all_courses = json.loads(all_courses)
+        for course in all_courses:
+            dep = course['科號'][5:9].replace(' ','')
+            Course.objects.get_or_create(
+                course_no = course['科號'],
+                defaults = {
+                    'course_no': course['科號'],
+                    'title_tw': course['課程中文名稱'],
+                    'title_en': course['課程英文名稱'],
+                    'credit': course['學分數'],
+                    'teacher': course['授課教師'],
+                    'department': dep,
+                }
+            )
 
     
