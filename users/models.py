5b8144e12f902ac9d7b7a0f5eecb00c601cb0c94
kadai <kadai0308@gmail.com>
2017-02-03 20:43:59 +0800

    first login auto create nickname

--- users/models.py
+++ users/models.py
@@ -15,6 +15,7 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender = User)
 def save_user_profile(sender, instance, **kwargs):
+    instance.profile.nickname = instance.email.split('@')[0]
     instance.profile.save()
 
 class Profile (models.Model):
