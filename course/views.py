850df57499a4a1d8dd39aff07ec8db6f7fe98241
kadai <kadai0308@gmail.com>
2017-03-12 20:39:28 +0800

    try to recover

--- course/views.py
+++ course/views.py
@@ -15,7 +15,7 @@ from bs4 import BeautifulSoup
 import re
 import html
 from rq import Queue
-from django_rq import job
+# from django_rq import job
 
 from nthu_plus import settings
 
@@ -135,9 +135,43 @@ def import_course_score_range (request):
     return JsonResponse('success', safe = False)
 
 
-@job
-def add_course_func ():
+# @job
+# def add_course_func ():
 
+#     years = range(99, 106)
+#     semesters = [10, 20]
+
+#     for year in years:
+#         for semester in semesters:
+#             url = 'https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/JH/OPENDATA/open_course_data_{:0>3}{}.json'.format(year, semester)
+#             all_courses = requests.get(url).text
+#             all_courses = json.loads(all_courses)
+#             for course in all_courses:
+#                 dep = course['科號'][5:9].replace(' ','')
+#                 # get_or_create return (object, created)
+#                 print (year, semester,course['課程中文名稱'])
+#                 course_in_db = Course.objects.get_or_create(
+#                         title_tw = course['課程中文名稱'],
+#                         teacher = course['授課教師'],
+#                         defaults = {
+#                             'title_tw': html.unescape(course['課程中文名稱']),
+#                             'title_en': course['課程英文名稱'],
+#                             'credit': course['學分數'],
+#                             'teacher': html.unescape(course['授課教師']),
+#                             'department': dep,
+#                         }
+#                     )
+#                 course_in_db[0].coursebyyear_set.get_or_create(
+#                         course_no = course['科號'],
+#                         defaults = {
+#                             'course_no': course['科號'],
+#                             'room_and_time': course['教室與上課時間'],
+#                         }
+#                     )    
+# add_course_func.delay()
+
+def add_course (request):    
+    add_course_func
     years = range(99, 106)
     semesters = [10, 20]
 
@@ -167,41 +201,7 @@ def add_course_func ():
                             'course_no': course['科號'],
                             'room_and_time': course['教室與上課時間'],
                         }
-                    )    
-add_course_func.delay()
-
-def add_course (request):    
-    add_course_func
-    # years = range(99, 106)
-    # semesters = [10, 20]
-
-    # for year in years:
-    #     for semester in semesters:
-    #         url = 'https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/JH/OPENDATA/open_course_data_{:0>3}{}.json'.format(year, semester)
-    #         all_courses = requests.get(url).text
-    #         all_courses = json.loads(all_courses)
-    #         for course in all_courses:
-    #             dep = course['科號'][5:9].replace(' ','')
-    #             # get_or_create return (object, created)
-    #             print (year, semester,course['課程中文名稱'])
-    #             course_in_db = Course.objects.get_or_create(
-    #                     title_tw = course['課程中文名稱'],
-    #                     teacher = course['授課教師'],
-    #                     defaults = {
-    #                         'title_tw': html.unescape(course['課程中文名稱']),
-    #                         'title_en': course['課程英文名稱'],
-    #                         'credit': course['學分數'],
-    #                         'teacher': html.unescape(course['授課教師']),
-    #                         'department': dep,
-    #                     }
-    #                 )
-    #             course_in_db[0].coursebyyear_set.get_or_create(
-    #                     course_no = course['科號'],
-    #                     defaults = {
-    #                         'course_no': course['科號'],
-    #                         'room_and_time': course['教室與上課時間'],
-    #                     }
-    #                 )
+                    )
 
 def add_course_worker (request):
 
