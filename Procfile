7b58e1ba0a2757663483693cc2e5f1fd682a4eb7
kadai <kadai0308@gmail.com>
2017-03-12 19:57:11 +0800

    set the django rq

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn nthu_plus.wsgi
+worker: python nthu_plus/manage.py rqworker high default low
--- course/views.py
+++ course/views.py
@@ -134,8 +134,9 @@ def import_course_score_range (request):
 
     return JsonResponse('success', safe = False)
 
+
 @job
-def add_course (request):    
+def add_course_func ():
 
     years = range(99, 106)
     semesters = [10, 20]
@@ -166,7 +167,41 @@ def add_course (request):
                             'course_no': course['科號'],
                             'room_and_time': course['教室與上課時間'],
                         }
-                    )
+                    )    
+add_course_func.delay()
+
+def add_course (request):    
+    add_course_func
+    # years = range(99, 106)
+    # semesters = [10, 20]
+
+    # for year in years:
+    #     for semester in semesters:
+    #         url = 'https://www.ccxp.nthu.edu.tw/ccxp/INQUIRE/JH/OPENDATA/open_course_data_{:0>3}{}.json'.format(year, semester)
+    #         all_courses = requests.get(url).text
+    #         all_courses = json.loads(all_courses)
+    #         for course in all_courses:
+    #             dep = course['科號'][5:9].replace(' ','')
+    #             # get_or_create return (object, created)
+    #             print (year, semester,course['課程中文名稱'])
+    #             course_in_db = Course.objects.get_or_create(
+    #                     title_tw = course['課程中文名稱'],
+    #                     teacher = course['授課教師'],
+    #                     defaults = {
+    #                         'title_tw': html.unescape(course['課程中文名稱']),
+    #                         'title_en': course['課程英文名稱'],
+    #                         'credit': course['學分數'],
+    #                         'teacher': html.unescape(course['授課教師']),
+    #                         'department': dep,
+    #                     }
+    #                 )
+    #             course_in_db[0].coursebyyear_set.get_or_create(
+    #                     course_no = course['科號'],
+    #                     defaults = {
+    #                         'course_no': course['科號'],
+    #                         'room_and_time': course['教室與上課時間'],
+    #                     }
+    #                 )
 
 def add_course_worker (request):
 
