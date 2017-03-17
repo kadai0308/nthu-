7d15edfa5a2bc6e94786237d52aeea63690fc9fc
kadai <kadai0308@gmail.com>
2017-03-17 14:56:55 +0800

    fix channels route setting error

--- nthu_plus/settings.py
+++ nthu_plus/settings.py
@@ -196,7 +196,7 @@ CHANNEL_LAYERS = {
         "CONFIG": {
             "hosts": [os.environ.get('REDISTOGO_URL', 'redis://localhost:6379')],
         },
-        "ROUTING": "chat.routing.channel_routing",
+        "ROUTING": "course.channels.routing.channel_routing",
     },
 }
 
