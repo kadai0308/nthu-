726b018518942090a7d2f7e8aa5b153f8a5778a2
kadai0308 <kadai0308@gmail.com>
2017-07-21 02:41:18 +0800

    edit allauth redirect url

--- users/adapter.py
+++ users/adapter.py
@@ -4,5 +4,5 @@ from allauth.account.adapter import DefaultAccountAdapter
 class MyAccountAdapter(DefaultAccountAdapter):
 
     def get_login_redirect_url(self, request):
-        path = "/users/{id}"
+        path = "https://nthu-plus.com/users/{id}"
         return path.format(id = request.user.id)
